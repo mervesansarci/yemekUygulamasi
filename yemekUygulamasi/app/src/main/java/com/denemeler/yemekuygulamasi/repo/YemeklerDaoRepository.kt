5340ef5af9d9b05f5d9bd67ae4a7d6c036077e43
Merve Sansarcı <mervesansarci@hotmail.com>
2022-02-20 09:23:00 +0300

    second commit

--- yemekUygulamasi/app/src/main/java/com/denemeler/yemekuygulamasi/repo/YemeklerDaoRepository.kt
+++ yemekUygulamasi/app/src/main/java/com/denemeler/yemekuygulamasi/repo/YemeklerDaoRepository.kt
@@ -38,7 +38,7 @@ class YemeklerDaoRepository {
     fun sepeteEkle(yemek_adi:String, yemek_resim_adi: String, yemek_fiyat: Int, yemek_siparis_adet: Int, kullanici_adi: String){
         fdao.siparisEkle(yemek_adi,yemek_resim_adi,yemek_fiyat,yemek_siparis_adet,kullanici_adi).enqueue(object : Callback<CRUDCevap>{
             override fun onResponse(call: Call<CRUDCevap>?, response: Response<CRUDCevap>?) {
-                Log.e("Sepete", "eklendi")
+                Log.e("sepet", " sepete eklendi")
             }
             override fun onFailure(call: Call<CRUDCevap>?, t: Throwable?) {} })
     }
