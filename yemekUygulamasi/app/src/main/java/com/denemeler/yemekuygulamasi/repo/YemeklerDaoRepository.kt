633170076ff3ba8ae21ff8bd358e4d98a24a16d5
Merve Sansarcı <mervesansarci@hotmail.com>
2022-02-20 09:52:23 +0300

    third commit
    third commit

--- yemekUygulamasi/app/src/main/java/com/denemeler/yemekuygulamasi/repo/YemeklerDaoRepository.kt
+++ yemekUygulamasi/app/src/main/java/com/denemeler/yemekuygulamasi/repo/YemeklerDaoRepository.kt
@@ -3,10 +3,7 @@ package com.denemeler.yemekuygulamasi.repo
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import com.denemeler.yemekuygulamasi.entity.CRUDCevap
-import com.denemeler.yemekuygulamasi.entity.Sepet
-import com.denemeler.yemekuygulamasi.entity.Yemekler
-import com.denemeler.yemekuygulamasi.entity.YemeklerCevap
+import com.denemeler.yemekuygulamasi.entity.*
 import com.denemeler.yemekuygulamasi.retrofit.ApiUtils
 import com.denemeler.yemekuygulamasi.retrofit.YemeklerDaoInterface
 import retrofit2.Call
@@ -33,17 +30,33 @@ class YemeklerDaoRepository {
 
     fun sepettekiYemekleriGetir(kullanici_adi:String){
 
+        fdao.tumSiparisler(kullanici_adi).enqueue(object : Callback<SepetCevap>{
+            override fun onResponse(call: Call<SepetCevap>, response: Response<SepetCevap>) {
+                val list = response.body().sepet_yemekler as ArrayList<Sepet>
+                sepetListesi.postValue(list)
+            }
+            override fun onFailure(call: Call<SepetCevap>?, t: Throwable?) {}
+
+        })
+
 
     }
     fun sepeteEkle(yemek_adi:String, yemek_resim_adi: String, yemek_fiyat: Int, yemek_siparis_adet: Int, kullanici_adi: String){
         fdao.siparisEkle(yemek_adi,yemek_resim_adi,yemek_fiyat,yemek_siparis_adet,kullanici_adi).enqueue(object : Callback<CRUDCevap>{
             override fun onResponse(call: Call<CRUDCevap>?, response: Response<CRUDCevap>?) {
-                Log.e("sepet", " sepete eklendi")
             }
             override fun onFailure(call: Call<CRUDCevap>?, t: Throwable?) {} })
     }
 
-    fun sepettenYemekSil(){
+    fun sepettenYemekSil(sepet_yemek_id:Int, kullanici_adi: String){
+        fdao.siparisSil(sepet_yemek_id,kullanici_adi).enqueue(object :Callback<CRUDCevap>{
+            override fun onResponse(call: Call<CRUDCevap>?, response: Response<CRUDCevap>?) {
+                sepettekiYemekleriGetir(kullanici_adi)
+            }
+
+            override fun onFailure(call: Call<CRUDCevap>?, t: Throwable?) {
+            }
+        })
 
     }
 
